6e475b4e40826daa4f4fbb2f98f5fa0d0e7af191
NilsenLukas <lukas.nilsen2021lax@gmail.com>
2025-04-07 14:25:32 -0400

    correction report creation bug

--- server/routes/correction-report.js
+++ server/routes/correction-report.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
-const { correctionReportCollection, eventCollection, userCollection } = require('../mongo');
+const { correctionReportCollection, eventCollection, userCollection, notificationCollection } = require('../mongo');
 // Nodemailer transporter setup
 const transporter = nodemailer.createTransport({
   service: 'gmail',
